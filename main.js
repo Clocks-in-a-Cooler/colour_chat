8464801c4f912cb56c0e2dd313c0f67c11c742e0
clocks-in-a-cooler <34520271+Clocks-in-a-Cooler@users.noreply.github.com>
2019-01-05 23:21:19 -0500

    tweaked log messages. disconnect not working.

--- main.js
+++ main.js
@@ -9,52 +9,65 @@ var hostname = "192.168.0.14";
 var port     = 3000;
 
 app.get('/', function(req, res){
-    console.log("incoming connection from: " + (req.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress));
+    log("[notification] incoming connection from: " + (req.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress));
     
-    //if (!req.secure) {res.redirect('https://' + req.headers.host + req.url);}
     res.sendFile(__dirname + '/index.html');
-    //res.sendFile(__dirname + '/styles.css');
-    //res.sendFile(__dirname + '/script.js');
 });
 
+//so that the resources can be loaded.
 app.use(express.static(__dirname));
 
 io.on('connection', function(socket){
+    //notify everyone
+    io.emit('chat message', "somebody has entered this chatroom.");
+
     socket.on('chat message', function(msg){
     //processing
-    console.log(msg);
+    log("[message] " + msg);
     io.emit('chat message', msg);
   });
+});
+
+io.on('disconnect', function() {
+    log("[notification] somebody has disconnected.");
+    io.emit('chat message', "somebody has disconnected.");
 });
 
 http.listen(port, function(){
     console.log('listening on *:' + port);
 });
 
+//custom log function
+function log(msg) {
+    msg = msg.trim();
+    if (msg == "") {
+        return;
+    }
+    
+    console.log(get_date_time() + " " + msg);
+}
 
-/* ----------------- for reference -------------------------------
-var app  = require('express')();
-var http = require('http').Server(app);
-var io   = require('socket.io')(http);
-var port = process.env.PORT || 3000;
-var fs   = require('fs');
+//helper, courtesy of Stack Overflow
+function get_date_time() {
+    var date = new Date();
 
-app.get('/', function(req, res){
-  console.log("incoming connection from: " + (req.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress));
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
 
-  //if (!req.secure) {res.redirect('https://' + req.headers.host + req.url);}
-  res.sendFile(__dirname + '/index.html');
-});
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    console.log("message: " + msg);
-    io.emit('chat message', msg);
-  });
-});
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
-});
+    var year = date.getFullYear();
 
-*/
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+    return year + "/" + month + "/" + day + " " + hour + ":" + min + ":" + sec;
+
+}
