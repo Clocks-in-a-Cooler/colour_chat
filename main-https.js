b98044a1974f8256176a6a3c10596caf42a5ed7a
clocks-in-a-cooler <34520271+Clocks-in-a-Cooler@users.noreply.github.com>
2019-01-30 13:27:45 -0500

    added https stuff, for good measure. can't trust no one on the Internet.

---
+++ main-https.js
@@ -0,0 +1,173 @@
+//chat server
+var fs      = require("fs");
+var app     = require("express")();
+var express = require("express");
+var https   = require("https"); //.Server(app)
+var server  = https.createServer({
+    key: fs.readFileSync("certificates/privKey.pem"),
+    cert: fs.readFileSync("certificates/newcert.pem"),
+    //requestCert: false,
+    //rejectUnauthorized: false,
+}, app);
+
+server.listen(3000, function() {
+    log("https listening on port: 3000.");
+});
+
+var io   = require("socket.io").listen(server);
+var port = 3000;
+
+var private_key = fs.readFileSync('certificates/privKey.pem').toString();
+var certificate = fs.readFileSync('certificates/newcert.pem').toString();
+
+var options = { key: private_key, cert: certificate };
+
+var users = {
+    //empty object
+    
+    add_user: function(id, name, colour) {
+        this[id] = new User(name, colour);
+        
+        log("[notification] a new user created: \n    id: " + id + "\n    name: " + name + "\n    colour: " + colour);
+    },
+    
+    get_user: function(id) {
+        return this[id];
+    },
+    
+    remove_user: function(id) {
+        delete this[id];
+    },
+};
+
+app.get('/', function(req, res){
+    //res.redirect('https://' + req.headers.host + req.url);
+
+    log("[notification] incoming connection from: " + (req.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress));
+    
+    res.sendFile(__dirname + '/index.html');
+});
+
+//so that the resources can be loaded.
+app.use(express.static(__dirname));
+
+io.sockets.on('connection', function(socket) {
+    //login event
+    socket.on('login', function(name) {
+        var new_colour = get_colour();
+        
+        users.add_user(socket.id, name, new_colour);
+    
+        log("[notification] " + name + " has connected to the chat server from " + socket.handshake.address + "\n    socket_id: " + socket.id);
+        io.emit('colour', new_colour);
+        io.emit('notification', name + " has connected to this server. welcome!");
+    });
+
+    socket.on('chat message', function(msg) {
+        //processing
+        var message = JSON.parse(msg);
+        log("[message] " + message.name + ": " + message.content);
+        io.emit('chat message', msg);
+    });
+    
+    socket.on('disconnect', function() {
+        var user = users.get_user(socket.id);
+        
+        if (user) {
+            log("[notification] " + user.name + " has disconnected.");
+            io.emit('notification', user.name + " has disconnected.");
+            
+            chat_colours[user.colour] = true; //free up that precious colour!
+        } else {
+            log("[notification] " + socket.id + " has disconnected.");
+        }
+        //more processing after. you'd think we'd be done, but no
+        
+        users.remove_user(socket.id);
+    });
+});
+
+/*
+https.createServer(options, app).listen(port, function() {
+    log("https server listening on port: " + port);
+}); */
+
+//custom log function
+function log(msg) {
+    msg = msg.trim();
+    if (msg == "") {
+        return;
+    }
+    
+    console.log(get_date_time() + " " + msg);
+}
+
+//helper, courtesy of Stack Overflow
+function get_date_time() {
+    var date = new Date();
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+    return year + "/" + month + "/" + day + " " + hour + ":" + min + ":" + sec;
+}
+
+/* ------------------------------------------------------------------------------------------------------- */
+//the colour part of this
+//colours for the chat box
+var chat_colours = {
+    "mediumvioletred": true,
+    "crimson": true,
+    "orangered": true,
+    "darkorange": true,
+    "gold": true,
+    "darkkhaki": true,
+    "sienna": true,
+    "chocolate": true,
+    "limegreen": true,
+    "forestgreen": true,
+    "lightseagreen": true,
+    "steelblue": true,
+    "royalblue": true,
+    "plum": true,
+    "orchid": true,
+    "darkslateblue": true,
+};
+
+function get_colour() {
+    var colours = Object.getOwnPropertyNames(chat_colours).filter(function(c) {return chat_colours[c]});
+    
+    if (colours.length == 0) {
+        colours = Object.getOwnPropertyNames(chat_colours);
+    }
+    
+    var colour = colours[Math.floor(Math.random() * colours.length)];
+    
+    //log("[notification] colour chosen: " + colour);
+    
+    chat_colours[colour] = false;
+    
+    return colour;
+}
+
+/* ------------------------------------------------------------------------------------------------------------ */
+//user type
+
+function User(name, colour) {
+    this.colour = colour;
+    this.name   = name;
+}
